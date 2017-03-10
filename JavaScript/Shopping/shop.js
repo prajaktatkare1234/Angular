c5f2ca201b7468a57721965f1dabacbc317db85e
SoumyaPotdar <soumya.potdar2>
2017-03-10 16:28:57 +0530

    commit

--- JavaScript/Shopping/shop.js
+++ JavaScript/Shopping/shop.js
@@ -6,7 +6,7 @@ function add()
   var b=parseInt(document.getElementById("q").value);
   var l= parseInt(myList.length);
  var x=myList.push([a,b]);
- document.getElementById("demo1").innerHTML=myList.toString();
+// document.getElementById("demo1").innerHTML=myList.toString();
 
 // document.getElementById("demo").innerHTML=x;
 
@@ -14,18 +14,18 @@ function add()
 function remove()
 {var l= parseInt(myList.length);
   myList.pop();
-document.getElementById("demo1").innerHTML=myList.toString();
+//document.getElementById("demo1").innerHTML=myList.toString();
 }
  function sh()
  {
    myList.shift();
-   document.getElementById("demo1").innerHTML=myList.toString();
+  // document.getElementById("demo1").innerHTML=myList.toString();
  }
  function unsh()
- {var p=document.getElementById("unf").value;
- var q=parseInt(document.getElementById("unq").value);
+ {var p=document.getElementById("f").value;
+ var q=parseInt(document.getElementById("q").value);
    myList.unshift([p,q]);
-   document.getElementById("demo1").innerHTML=myList.toString();
+   //document.getElementById("demo1").innerHTML=myList.toString();
 }
 function display()
 {
